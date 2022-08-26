7e4c1c2df64ee2439a0beb4b90ded37b50e350c0
eliel <elieldscalore@gmail.com>
2022-08-26 13:51:46 -0300

    exercises 2.8 ok

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,27 +1,40 @@
 /*import ShowNote from "./example/showNote/showNote";*/
 import { useState } from "react";
 
+/////////////////////////////////////////Modificações á partir daqui
+
 function App() {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
-  const [newName, setNewName] = useState("");
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "999-999" },
+  ]);
+
+  const [newName, setNewName] = useState({ name: "", number: "" });
 
   const handleChange = (e) => {
-    setNewName(e.target.value);
+    setNewName({ ...newName, [e.target.name]: e.target.value });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     if (
-      persons.filter(
-        (currentName) =>
-          currentName.name.toLowerCase() === newName.toLowerCase()
-      ).length > 0
+      persons.filter((currentName) => currentName.name === newName.name)
+        .length > 0
     ) {
-      return alert(`${newName} is already added to phonebook`);
+      return alert(`${newName.name} is already added to phonebook`);
+    }
+    if (
+      persons.filter((currentName) => currentName.number === newName.number)
+        .length > 0
+    ) {
+      return alert(`${newName.number} is already added to phonebook`);
     }
 
-    setPersons([...persons, { name: newName }]);
+    setPersons([
+      ...persons,
+      { name: newName.name },
+      { number: newName.number },
+    ]);
   };
 
   return (
@@ -29,20 +42,38 @@ function App() {
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input onChange={handleChange} />
+          name:{" "}
+          <input
+            onChange={handleChange}
+            type="text"
+            name="name"
+            value={persons.name}
+          />
+          number:
+          <input
+            onChange={handleChange}
+            type="text"
+            name="number"
+            value={persons.number}
+          />
         </div>
         <div>
           <button onClick={handleSubmit} type="submit">
             add
           </button>
         </div>
-        <div>debug: {newName}</div>
+        <div>
+          debug: {newName.name} {newName.number}
+        </div>
       </form>
       <h2>Numbers</h2>
+
       {persons.map((currentElement) => {
         return (
           <div key={currentElement.name}>
-            <strong>{currentElement.name}</strong>
+            <strong>
+              {currentElement.name} {currentElement.number}
+            </strong>
           </div>
         );
       })}

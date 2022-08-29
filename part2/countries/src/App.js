a25c61b3d4958419e7433cde9b4c8a5c5f1456d9
eliel <elieldscalore@gmail.com>
2022-08-29 16:15:22 -0300

    exercise 2.12 ok

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-function App() {
+export function App() {
+  const [countries, setCountries] = useState([]);
+  const [search, SetSearch] = useState("");
+  const [countriesSearch, setCountriesSearch] = useState(countries);
+
+  useEffect(() => {
+    axios.get("https://restcountries.com/v3.1/all").then((response) => {
+      setCountries(response.data);
+    });
+  }, []);
+
+  const handleChangeSearch = (e) => {
+    SetSearch(e.target.value);
+
+    setCountriesSearch(
+      countries.filter((current) =>
+        current.name.common.toLowerCase().includes(search.toLowerCase())
+      )
+    );
+    console.log(countriesSearch);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        Find Countries
+        <input
+          size="large"
+          type="text"
+          placeholder="input search text"
+          onChange={handleChangeSearch}
+          enterButton
+        />
+      </div>
+
+      <div>
+        {countriesSearch.length === 0 ? (
+          countries.map((current) => {
+            return (
+              <div>
+                <h2>{current.name.common}</h2>
+              </div>
+            );
+          })
+        ) : countriesSearch.length === 1 ? (
+          countriesSearch.map((cur) => {
+            let objKeys = Object.keys(countriesSearch[0].languages);
+
+            return (
+              <div>
+                <h1>{cur.name.common}</h1>
+                <p>
+                  <strong>Capital:</strong> {cur.capital}
+                </p>
+                <p>
+                  <strong>Area:</strong> {cur.area}
+                </p>
+                <p>
+                  <strong>Population:</strong> {cur.population}
+                </p>
+                <h2>Languages: </h2>
+                <ul>
+                  {objKeys.map((curObject) => {
+                    return <li>{countriesSearch[0].languages[curObject]}</li>;
+                  })}
+                </ul>
+                <img
+                  src={cur.flags.png}
+                  alt="
+country flag"
+                ></img>
+              </div>
+            );
+          })
+        ) : countriesSearch.length > 10 ? (
+          <div>Too Many matches, specify another filter</div>
+        ) : (
+          countriesSearch.map((currentCountries) => {
+            return (
+              <div>
+                <h2>{currentCountries.name.common}</h2>
+              </div>
+            );
+          })
+        )}
+      </div>
     </div>
   );
 }
-
-export default App;

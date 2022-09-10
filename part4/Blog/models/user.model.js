5b42692d0e21683cb0c71b84ec137ddb27031e48
eliel <elieldscalore@gmail.com>
2022-09-10 12:49:16 -0300

    exercise 4.16 ok

--- part4/Blog/models/user.model.js
+++ part4/Blog/models/user.model.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose")
 const {Schema, model} = require("mongoose")
 
 const UserSchema = new Schema ({
-    username: {type:String, required:true, trim: true},
-    name: {type: String, required: true, trim:true},
-    passwordHash: {type: String, required:true, trim:true}
+    username: {type:String, required:true, trim: true },
+    name: {type: String, required: true, trim:true, minLength: 3},
+    passwordHash: {type: String, required:true, trim:true, minLength: 3}
 })
 
 const UserModel = model("User", UserSchema);
